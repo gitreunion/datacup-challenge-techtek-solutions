d2d932d95a34e3edb5e81ca779c120a79b538490
MathTek <mathieu.rasoanaivo@epitech.eu>
2024-11-23 10:48:34 +0400

    Feat: change the map model

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -57,8 +57,8 @@ export default function Map() {
             });
             setMap(mapInstance);
 
-            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+            L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+                attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
             }).addTo(mapInstance);
 
             L.control.zoom({
@@ -98,7 +98,7 @@ export default function Map() {
                             select: 'id, idweb, filename, objet, dateparution, datefindiffusion, datelimitereponse, nomacheteur, donnees',
                             where: whereClause,
                             limit,
-                            order_by: 'datelimitereponse DESC',
+                            order_by: 'dateparution DESC',
                             offset,
                         },
                     }
